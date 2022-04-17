f5f88659205609b071f587a1de891a8ffde5158d
SydP <syd.pleno@gmail.com>
2022-04-17 15:43:53 +1000

    Update comments.

--- generate-overlay-specimens-linux.sh
+++ generate-overlay-specimens-linux.sh
@@ -50,6 +50,7 @@ create_test_image_file()
 
 
 # Unmounts the overlay filesystem and its different layers.
+#
 umount_overlay()
 {
     if mountpoint -q ${MOUNT_POINT}/overlay; then
@@ -66,8 +67,9 @@ umount_overlay()
     fi
 }
 
-# Creates test overlay files
-create_test_overlay_files()
+# Creates test overlay filesystem
+#
+create_test_overlay_filesystem()
 {
     IMAGE_FILE=$1;
     IMAGE_SIZE=$2;
@@ -92,7 +94,7 @@ create_test_overlay_files()
     mkdir ${MOUNT_POINT}/upper/upper
     mkdir ${MOUNT_POINT}/upper/workdir
 
-    # generate test data in lower layer of overlay
+    # generate test data in lower layer of overlay filesystem
     sudo chown $USER:$USER ${MOUNT_POINT}/lower
     echo 'aaaaaaaa' > ${MOUNT_POINT}/lower/a.txt
     echo '11111111' > ${MOUNT_POINT}/lower/1.txt
@@ -106,7 +108,7 @@ create_test_overlay_files()
     mkdir ${MOUNT_POINT}/lower/testdir
     echo 'bbbbbbbb' > ${MOUNT_POINT}/lower/testdir/b.txt
 
-    echo "Generated test data in lower layer."
+    echo "Generated test data in lower layer of overlay filesystem."
 
     # remount lower directory as read-only
     sudo mount -f -o remount,ro ${IMAGE_FILE}_lower.dd ${MOUNT_POINT}/lower;
@@ -114,7 +116,7 @@ create_test_overlay_files()
     # mount overlay filesystem
     sudo mount -t overlay -o lowerdir=${MOUNT_POINT}/lower,upperdir=${MOUNT_POINT}/upper/upper,workdir=${MOUNT_POINT}/upper/workdir none ${MOUNT_POINT}/overlay
 
-      # generate test data in the overlay (i.e. the upper layer)
+    # generate test data in the overlay filesystem (i.e. the upper layer)
     echo 'cccccccc' > ${MOUNT_POINT}/overlay/c.txt;
     echo 'dddddddd' > ${MOUNT_POINT}/overlay/testdir/d.txt;
 
@@ -128,7 +130,7 @@ create_test_overlay_files()
     mkdir ${MOUNT_POINT}/overlay/replacedir
     touch ${MOUNT_POINT}/overlay/replacedir/replace2.txt
 
-    echo "Generated test data in upper layer."
+    echo "Generated test data in upper layer of overlay filesystem."
 
     # generate a overlay file listing as seen by the operating system
     sudo ls -alRt ${MOUNT_POINT}/overlay > ${SPECIMENS_PATH}/overlay.txt;
@@ -144,6 +146,7 @@ create_test_overlay_files()
 assert_availability_binary dd;
 assert_availability_binary mkfs.ext4;
 assert_availability_binary mount;
+assert_availability_binary mountpoint;
 
 set -e;
 
@@ -152,4 +155,4 @@ MOUNT_POINT="tmp";
 
 mkdir -p ${SPECIMENS_PATH};
 
-create_test_overlay_files ${SPECIMENS_PATH}/overlay 10485760 512
+create_test_overlay_filesystem ${SPECIMENS_PATH}/overlay 10485760 512
