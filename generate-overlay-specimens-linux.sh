7b17c1ccba012c6d161cf49f93fe9ba27491c40f
sydp <syd.pleno@gmail.com>
2022-04-23 05:13:35 +1000

    Added XFS filesystem to specimens and clean up of scripts (#2)

--- generate-overlay-specimens-linux.sh
+++ generate-overlay-specimens-linux.sh
@@ -38,14 +38,15 @@ create_test_image_file()
     IMAGE_FILE1=$1;
     IMAGE_SIZE1=$2;
     SECTOR_SIZE1=$3;
-    shift 3;
+    FILE_SYSTEM=$4;
+    shift 4;
     local ARGUMENTS=("$@");
 
     sudo dd if=/dev/zero of=${IMAGE_FILE1} bs=${SECTOR_SIZE1} count=$(( ${IMAGE_SIZE1} / ${SECTOR_SIZE1})) 2> /dev/null;
     echo $? "Creating disk image ${IMAGE_FILE1}";
 
-    sudo mkfs.ext4 -F -q ${ARGUMENTS[@]} ${IMAGE_FILE1};
-    echo $? "Making EXT4 file system (${IMAGE_FILE1})";
+    sudo mkfs.${FILE_SYSTEM} -q ${ARGUMENTS[@]} ${IMAGE_FILE1};
+    echo $? "Making ${FILE_SYSTEM} file system (${IMAGE_FILE1})";
 }
 
 
@@ -65,11 +66,56 @@ umount_overlay()
         sudo umount ${MOUNT_POINT}/upper;
         echo "Unmounted ${MOUNT_POINT}/upper"
     fi
+    if mountpoint -q ${MOUNT_POINT}; then
+        sudo umount ${MOUNT_POINT};
+        echo "Unmounted ${MOUNT_POINT}"
+    fi
 }
 
-# Creates test overlay filesystem
+# generate test data in lower layer of overlay filesystem
+generate_lower_layer_data()
+{
+     sudo chown $USER:$USER ${MOUNT_POINT}/lower
+    echo 'aaaaaaaa' > ${MOUNT_POINT}/lower/a.txt
+    echo '11111111' > ${MOUNT_POINT}/lower/1.txt
+    setfattr -n "user.myxattr1" -v "lower extended attribute" ${MOUNT_POINT}/lower/1.txt
+
+    mkdir ${MOUNT_POINT}/lower/deletedir
+    touch ${MOUNT_POINT}/lower/deletedir/delete.txt
+
+    mkdir ${MOUNT_POINT}/lower/replacedir
+    touch ${MOUNT_POINT}/lower/replacedir/replace.txt
+
+    mkdir ${MOUNT_POINT}/lower/testdir
+    echo 'bbbbbbbb' > ${MOUNT_POINT}/lower/testdir/b.txt
+
+    echo "Generated test data in lower layer of overlay filesystem."
+}
+
+# generate test data in the overlay filesystem (i.e. the upper layer)
+generate_upper_layer_data()
+{
+    echo 'cccccccc' > ${MOUNT_POINT}/overlay/c.txt;
+    setfattr -n "user.myxattr1" -v "upper extended attribute" ${MOUNT_POINT}/overlay/c.txt
+
+    echo 'dddddddd' > ${MOUNT_POINT}/overlay/testdir/d.txt;
+
+    mkdir ${MOUNT_POINT}/overlay/newdir;
+    echo 'this is a test for overlay!' > ${MOUNT_POINT}/overlay/newdir/e.txt;
+
+    rm -rf ${MOUNT_POINT}/overlay/deletedir;
+    rm ${MOUNT_POINT}/overlay/1.txt;
+
+    rm -fr ${MOUNT_POINT}/overlay/replacedir;
+    mkdir ${MOUNT_POINT}/overlay/replacedir
+    touch ${MOUNT_POINT}/overlay/replacedir/replace2.txt
+
+    echo "Generated test data in upper layer of overlay filesystem."
+}
+
+# Creates test overlay filesystem with upper/lower layers on separate EXT4 filesystems
 #
-create_test_overlay_filesystem()
+create_multiple_overlay_filesystem()
 {
     IMAGE_FILE=$1;
     IMAGE_SIZE=$2;
@@ -94,21 +140,7 @@ create_test_overlay_filesystem()
     mkdir ${MOUNT_POINT}/upper/upper
     mkdir ${MOUNT_POINT}/upper/workdir
 
-    # generate test data in lower layer of overlay filesystem
-    sudo chown $USER:$USER ${MOUNT_POINT}/lower
-    echo 'aaaaaaaa' > ${MOUNT_POINT}/lower/a.txt
-    echo '11111111' > ${MOUNT_POINT}/lower/1.txt
-
-    mkdir ${MOUNT_POINT}/lower/deletedir
-    touch ${MOUNT_POINT}/lower/deletedir/delete.txt
-
-    mkdir ${MOUNT_POINT}/lower/replacedir
-    touch ${MOUNT_POINT}/lower/replacedir/replace.txt
-
-    mkdir ${MOUNT_POINT}/lower/testdir
-    echo 'bbbbbbbb' > ${MOUNT_POINT}/lower/testdir/b.txt
-
-    echo "Generated test data in lower layer of overlay filesystem."
+    generate_lower_layer_data
 
     # remount lower directory as read-only
     sudo mount -f -o remount,ro ${IMAGE_FILE}_lower.dd ${MOUNT_POINT}/lower;
@@ -116,35 +148,58 @@ create_test_overlay_filesystem()
     # mount overlay filesystem
     sudo mount -t overlay -o lowerdir=${MOUNT_POINT}/lower,upperdir=${MOUNT_POINT}/upper/upper,workdir=${MOUNT_POINT}/upper/workdir none ${MOUNT_POINT}/overlay
 
-    # generate test data in the overlay filesystem (i.e. the upper layer)
-    echo 'cccccccc' > ${MOUNT_POINT}/overlay/c.txt;
-    echo 'dddddddd' > ${MOUNT_POINT}/overlay/testdir/d.txt;
-
-    mkdir ${MOUNT_POINT}/overlay/newdir;
-    echo 'eeeeeeee' > ${MOUNT_POINT}/overlay/newdir/e.txt;
-
-    rm -rf ${MOUNT_POINT}/overlay/deletedir;
-    rm ${MOUNT_POINT}/overlay/1.txt;
-
-    rm -fr ${MOUNT_POINT}/overlay/replacedir;
-    mkdir ${MOUNT_POINT}/overlay/replacedir
-    touch ${MOUNT_POINT}/overlay/replacedir/replace2.txt
-
-    echo "Generated test data in upper layer of overlay filesystem."
+    generate_upper_layer_data
 
     # generate a overlay file listing as seen by the operating system
-    sudo ls -alRt ${MOUNT_POINT}/overlay > ${SPECIMENS_PATH}/overlay.txt;
-    sudo ls -alRt ${MOUNT_POINT}/lower > ${SPECIMENS_PATH}/lower.txt;
-    sudo ls -alRt ${MOUNT_POINT}/upper/upper > ${SPECIMENS_PATH}/upper.txt;
-    sudo ls -alRt ${MOUNT_POINT}/upper/workdir > ${SPECIMENS_PATH}/workdir.txt;
+    sudo ls -alRt ${MOUNT_POINT}/overlay > ${IMAGE_FILE}_multiple_overlay.txt;
+    sudo ls -alRt ${MOUNT_POINT}/lower > ${IMAGE_FILE}_multiple_lower.txt;
+    sudo ls -alRt ${MOUNT_POINT}/upper/upper > ${IMAGE_FILE}_multiple_upper.txt;
+    sudo ls -alRt ${MOUNT_POINT}/upper/workdir > ${IMAGE_FILE}_multiple_workdir.txt;
 
     umount_overlay
 }
 
+# Creates test overlay filesystem using a single EXT4 file system
+#
+create_single_overlay_filesystem()
+{
+    IMAGE_FILE=$1;
+    IMAGE_SIZE=$2;
+    SECTOR_SIZE=$3;
+    shift 3;
+    local ARGUMENTS=("$@");
+
+    umount_overlay
+
+    create_test_image_file ${IMAGE_FILE}.dd ${IMAGE_SIZE} ${SECTOR_SIZE} ${ARGUMENTS[@]};
+
+    rm -rf ${MOUNT_POINT}
+    mkdir -p ${MOUNT_POINT}
+
+    sudo mount -o loop,rw ${IMAGE_FILE}.dd ${MOUNT_POINT};
+    echo 'Overlay image file mounted'
+    mkdir -p ${MOUNT_POINT}/lower ${MOUNT_POINT}/upper ${MOUNT_POINT}/overlay ${MOUNT_POINT}/workdir
+
+    generate_lower_layer_data
+
+    # mount overlay filesystem
+    sudo mount -t overlay -o lowerdir=${MOUNT_POINT}/lower,upperdir=${MOUNT_POINT}/upper,workdir=${MOUNT_POINT}/workdir none ${MOUNT_POINT}/overlay
+
+    generate_upper_layer_data
+
+    # generate a overlay file listing as seen by the operating system
+    sudo ls -alRt ${MOUNT_POINT}/overlay > ${IMAGE_FILE}_single_overlay.txt;
+    sudo ls -alRt ${MOUNT_POINT}/lower > ${IMAGE_FILE}_single_lower.txt;
+    sudo ls -alRt ${MOUNT_POINT}/upper > ${IMAGE_FILE}_single_upper.txt;
+    sudo ls -alRt ${MOUNT_POINT}/workdir > ${IMAGE_FILE}_single_workdir.txt;
+
+    umount_overlay
+}
 
 # Check that the binaries used in this test exist
 assert_availability_binary dd;
 assert_availability_binary mkfs.ext4;
+assert_availability_binary mkfs.xfs;
 assert_availability_binary mount;
 assert_availability_binary mountpoint;
 
@@ -155,4 +210,7 @@ MOUNT_POINT="tmp";
 
 mkdir -p ${SPECIMENS_PATH};
 
-create_test_overlay_filesystem ${SPECIMENS_PATH}/overlay 10485760 512
+create_multiple_overlay_filesystem ${SPECIMENS_PATH}/overlay_ext4 10485760 512 ext4
+create_single_overlay_filesystem ${SPECIMENS_PATH}/overlay_ext4 10485760 512 ext4
+create_multiple_overlay_filesystem ${SPECIMENS_PATH}/overlay_xfs 20971520 512 xfs
+create_single_overlay_filesystem ${SPECIMENS_PATH}/overlay_xfs 20971520 512 xfs
